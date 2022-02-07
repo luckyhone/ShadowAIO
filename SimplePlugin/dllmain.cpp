3106894c974db851b3d3895c6a7379ad6c7280a1
cmclient <cmclient@pm.me>
2022-02-07 19:39:25 +0100

    removed garen

--- SimplePlugin/dllmain.cpp
+++ SimplePlugin/dllmain.cpp
@@ -3,11 +3,10 @@
 // Declare plugin name & supported champions
 //
 PLUGIN_NAME("ShadowAIO");
-SUPPORTED_CHAMPIONS(champion_id::Garen, champion_id::Tryndamere, champion_id::Kindred, champion_id::Trundle);
+SUPPORTED_CHAMPIONS(champion_id::Tryndamere, champion_id::Kindred, champion_id::Trundle);
 
 // Include champion file
 //
-#include "garen.h"
 #include "tryndamere.h"
 #include "kindred.h"
 #include "trundle.h"
@@ -24,32 +23,26 @@ PLUGIN_API bool on_sdk_load(plugin_sdk_core* plugin_sdk_good)
     //
     switch (myhero->get_champion())
     {
-    case champion_id::Garen:
-        // Load garen script
-        // champ from template (https://dev.bgx.gg/docs/champion)
-        //
-        garen::load();
-        break;
-    case champion_id::Tryndamere:
-        // Load tryndamere script
-        //
-        tryndamere::load();
-        break;
-    case champion_id::Kindred:
-        // Load kindred script
-        //
-        kindred::load();
-        break;
-    case champion_id::Trundle:
-        // Load trundle script
-        //
-        trundle::load();
-        break;
-    default:
-        // We don't support this champ, print message and return false (core will not load this plugin and on_sdk_unload will be never called)
-        //
-        console->print("[ShadowAIO] Champion %s is not supported in ShadowAIO!", myhero->get_model_cstr());
-        return false;
+        case champion_id::Tryndamere:
+            // Load tryndamere script
+            //
+            tryndamere::load();
+            break;
+        case champion_id::Kindred:
+            // Load kindred script
+            //
+            kindred::load();
+            break;
+        case champion_id::Trundle:
+            // Load trundle script
+            //
+            trundle::load();
+            break;
+        default:
+            // We don't support this champ, print message and return false (core will not load this plugin and on_sdk_unload will be never called)
+            //
+            console->print("[ShadowAIO] Champion %s is not supported in ShadowAIO!", myhero->get_model_cstr());
+            return false;
     }
 
     // Return success, our plugin will be loaded now
@@ -64,27 +57,22 @@ PLUGIN_API void on_sdk_unload()
 {
     switch (myhero->get_champion())
     {
-    case champion_id::Garen:
-        // Unload garen script
-        //
-        garen::unload();
-        break;
-    case champion_id::Tryndamere:
-        // Unload tryndamere script
-        //
-        tryndamere::unload();
-        break;
-    case champion_id::Kindred:
-        // Unload kindred script
-        //
-        kindred::unload();
-        break;
-    case champion_id::Trundle:
-        // Unload trundle script
-        //
-        trundle::unload();
-        break;
-    default:
-        break;
+        case champion_id::Tryndamere:
+            // Unload tryndamere script
+            //
+            tryndamere::unload();
+            break;
+        case champion_id::Kindred:
+            // Unload kindred script
+            //
+            kindred::unload();
+            break;
+        case champion_id::Trundle:
+            // Unload trundle script
+            //
+            trundle::unload();
+            break;
+        default:
+            break;
     }
 }
